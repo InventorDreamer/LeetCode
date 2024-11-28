2f6c200c385883dd7008c938c71d69b26182f4af
Daniil <130999833+InventorDreamer@users.noreply.github.com>
2024-11-28 22:06:59 +0300

    Update Recyclable_and _Low_Fat_Products.sql

--- SQL/EASY_LEVEL/Recyclable_and _Low_Fat_Products.sql
+++ SQL/EASY_LEVEL/Recyclable_and _Low_Fat_Products.sql
@@ -24,9 +24,18 @@ Output:
 +-------------+
 '''
 
+'SQL'
 SELECT
     product_id
 FROM
     Products
 WHERE
     low_fats = 'Y' AND recyclable = 'Y'
+
+'Pandas'
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    result = products[(products['low_fats']=='Y') & (products['recyclable']=='Y')]
+    return result[['product_id']]
+
